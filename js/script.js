604a39013684a7055b197b66da764cc04d1c98b4
HerrWal <85037202+HerrWal@users.noreply.github.com>
2021-08-09 19:35:49 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,7 +11,48 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+const quotes = [
+  {
+    quote : "To succeed in life, you need two things: ignorance and confidence.",
+    source : "Mark Twain"
+  },
+  {
+    quote : "The best revenge is massive success.",
+    source : "Frank Sinatra"
+  },
+  {
+    quote : "I walk slowly but never backwards.",
+    source : "Abraham Lincoln" 
+  },
+  {
+    quote : "Life is not measured by the number of reaths we take, but by the moments that take our breath away",
+    source : "Maya Angelou"
+  },
+  {
+    quote : "The path to success is to take massive determined action.",
+    source : "Tony Robbins"
+  },
+  {
+    quote : "A strong positive self-image is the best possible preparation for success.",
+    source : "Dr. Joyce Brothers"
+  },
+  {
+    quote : "There are no traffic jams along the extra mile.",
+    source : "Roger Stauback"
+  },
+  {
+    quote : "I attribute my success to this: I never took nor gave any excuses.",
+    source : "Florence Nightingale"
+  },
+  {
+    quote : "It is never too late to be what you might have been.",
+    source : "George Eliot"
+  },
+  {
+    quote : "It is not the strongest species that survive, nor the most intelligent, but the most responsive to change.",
+    source : "Charles Darwin"
+  }
+]
 
 /***
  * `getRandomQuote` function
